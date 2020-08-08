2f004407f986336b0a2bf06c4fc1d6ef9e2e7ade
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2020-08-08 13:04:08 +0200

    feat: Adds ajax fetch requests for Venue and Artist deletion
    Change log:
    	* script.js - Added deleteVenue and deleteArtist requests

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,29 @@ window.parseISOString = function parseISOString(s) {
   var b = s.split(/\D+/);
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 };
+
+function deleteVenue(e) {
+  const id = e.currentTarget.dataset.id;
+  fetch(`/venues/${id}/delete`, { method: 'DELETE' })
+    .then(response => {
+      if (response.ok) {
+        console.log("SUCCESS:", response);
+        window.location.href = "/";
+      } else {
+        console.log("ERROR:", response);
+      }
+    })
+};
+
+function deleteArtist(e) {
+  const id = e.currentTarget.dataset.id;
+  fetch(`/artists/${id}/delete`, { method: 'DELETE' })
+    .then(response => {
+      if (response.ok) {
+        console.log("SUCCESS:", response);
+        window.location.href = "/";
+      } else {
+        console.log("ERROR:", response);
+      }
+    })
+};
