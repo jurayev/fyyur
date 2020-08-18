e3ce81a75dec82804fa4fd98aeb4a10ffa9379a1
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2020-08-18 16:53:33 +0200

    chore: adds heroku config and gunicorn dependencies

--- config.py
+++ config.py
@@ -3,18 +3,17 @@ from utils import format_datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-from sqlalchemy_utils import database_exists, create_database
 
 
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
-DATABASE_URI = 'postgres://postgres:postgres@localhost:5432/fyyur'
+LOCAL_DATABASE_URI = 'postgres://postgres:postgres@localhost:5432/fyyur'
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.urandom(32)
+app.config['SECRET_KEY'] = 'EVsPENx_0Voj7IQ2Xs0Qug'
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', LOCAL_DATABASE_URI)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['WTF_CSRF_CHECK_DEFAULT'] = False
@@ -26,6 +25,3 @@ csrf = CSRFProtect()
 csrf.init_app(app)
 
 db = SQLAlchemy(app)
-
-if not database_exists(DATABASE_URI):
-    create_database(DATABASE_URI)
